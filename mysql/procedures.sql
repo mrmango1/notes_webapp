ad3185749461d0d344e974997845829e30a584e4
Anderson Grefa <65833915+mrmango1@users.noreply.github.com>
2022-08-29 01:42:49 +0000

    fix: change name from person to user and improve some relasionships

--- mysql/procedures.sql
+++ mysql/procedures.sql
@@ -13,7 +13,7 @@
 DELIMITER &&  
 CREATE PROCEDURE userTables (int id)
 BEGIN    
-  select * from table where id_person=id
+  select * from table where id_user=id
 END &&  
 DELIMITER ;   
 
--- mysql/to-do.sql
+++ mysql/to-do.sql
@@ -15,79 +15,66 @@ CREATE SCHEMA IF NOT EXISTS `to-do` ;
 USE `to-do` ;
 
 -- -----------------------------------------------------
--- Table `to-do`.`person`
+-- Table `user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `to-do`.`person` (
-  `id_person` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` INT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(40) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_person`))
+  PRIMARY KEY (`id_user`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `to-do`.`table`
+-- Table `table`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `to-do`.`table` (
+CREATE TABLE IF NOT EXISTS `table` (
   `id_table` INT NOT NULL AUTO_INCREMENT,
-  `id_person` INT NOT NULL,
+  `id_user` INT NOT NULL,
   `title` VARCHAR(45) NOT NULL DEFAULT 'My Table',
   `description` VARCHAR(200) NULL,
   `color` VARCHAR(7) NULL,
-  PRIMARY KEY (`id_table`, `id_person`),
-  CONSTRAINT `fk_table_person`
-    FOREIGN KEY (`id_person`)
-    REFERENCES `to-do`.`person` (`id_person`))
+  PRIMARY KEY (`id_table`, `id_user`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `to-do`.`task`
+-- Table `task`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `to-do`.`task` (
+CREATE TABLE IF NOT EXISTS `task` (
   `id_task` INT NOT NULL AUTO_INCREMENT,
   `id_table` INT NOT NULL,
   `title` VARCHAR(45) NOT NULL DEFAULT 'My Task',
   `description` VARCHAR(200) NULL DEFAULT 'Sin descripcion',
   `importance` TINYINT NULL DEFAULT 0,
   `created_at` DATETIME NULL DEFAULT now(),
-  `update_at` DATETIME NULL DEFAULT now(),
   `limit_date` DATETIME NULL,
   `done` TINYINT NULL DEFAULT 0,
-  PRIMARY KEY (`id_task`, `id_table`),
-  CONSTRAINT `fk_task_table1`
-    FOREIGN KEY (`id_table`)
-    REFERENCES `to-do`.`table` (`id_table`))
+  PRIMARY KEY (`id_task`, `id_table`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `to-do`.`topic`
+-- Table `topic`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `to-do`.`topic` (
+CREATE TABLE IF NOT EXISTS `topic` (
   `id_topic` INT NOT NULL AUTO_INCREMENT,
+  `id_table` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `description` VARCHAR(200) NULL,
-  PRIMARY KEY (`id_topic`))
+  PRIMARY KEY (`id_topic`, `id_table`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `to-do`.`task_has_topic`
+-- Table `topic_has_task`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `to-do`.`task_has_topic` (
-  `id_task` INT NOT NULL,
-  `id_table` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `topic_has_task` (
   `id_topic` INT NOT NULL,
-  PRIMARY KEY (`id_task`, `id_table`, `id_topic`),
-  CONSTRAINT `fk_task_has_topic_task1`
-    FOREIGN KEY (`id_task` , `id_table`)
-    REFERENCES `to-do`.`task` (`id_task` , `id_table`),
-  CONSTRAINT `fk_task_has_topic_topic1`
-    FOREIGN KEY (`id_topic`)
-    REFERENCES `to-do`.`topic` (`id_topic`))
+  `id_task` INT NOT NULL,
+  PRIMARY KEY (`id_topic`, `id_task`))
 ENGINE = InnoDB;
 
 
