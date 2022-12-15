9c8125ba7a0b52f4837165e7d032984a6fb992f4
MPaulaG <mpaula_12@live.com>
2022-12-15 18:15:30 -0500

    Logout

--- src/main/java/com/itsqmet/todo/servlet/Logout.java
+++ src/main/java/com/itsqmet/todo/servlet/Logout.java
@@ -2,7 +2,6 @@ package com.itsqmet.todo.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +13,7 @@ import java.io.IOException;
 public class Logout extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     // invalidate the session if exists
